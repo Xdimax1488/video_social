c0fa8f633696cd9b86d2b24a6e8c2fdd06072039
Dima <dima_kurinnoy@mail.ru>
2022-01-06 23:49:15 +0200

    make form for create video and comment

--- video_social/frontend/src/components/create_vpost/create_post.js
+++ video_social/frontend/src/components/create_vpost/create_post.js
@@ -1,67 +1,97 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-import './create_post.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookie from 'react-cookie';
+
+import './create_post.css';
 
 function CreatePost() {
-    
+  const url = 'http://127.0.0.1:8000/api/videos/';
+  const [date, setDate] = useState({
+    name: '',
+    description: '',
+    video: '',
+    preview: '',
+    category: '2',
+    author: '1',
+  });
+  function submit(event) {
+    event.preventDefault();
+    axios
+      .post(url, {
+        name: date.name,
+        description: date.description,
+        video: date.value,
+        preview: date.preview,
+        category: date.category,
+        author: date.author,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
 
-    function handleFormSubmit  (event){
-        event.preventDefault();
-        const name =event.target.elements.name.value;
-        const description =event.target.elements.description.value;
-        const video =event.target.elements.video.value;
-        const preview =event.target.elements.preview.value;
-        
-        console.log(name,description,video,preview)
-        axios({
-            method:'POST',
-            url:"http://127.0.0.1:8000/api/videos/",
-            date:{
-                name:name,
-                description:description,
-                video:video,
-                preview:preview,
-                category:"2",
-                author: "1"
-                
-            }
-        })
-    
-    
+  function handle(e) {
+    const newdate = { ...date };
+    newdate[e.target.id] = e.target.value;
+    setDate(newdate);
+    console.log(newdate);
+  }
 
-        
-
-    }
-
-    return (
-        <div className="content">
-          <div className="conteiner__category">
-            <form className="create__form" onSubmit={(event)=>handleFormSubmit(event)}>
-                <ul>
-                    <li>
-                       <label for= "name">Название:</label>
-                       <input className="input__text" type="text" id="name" name="name"/>
-                    </li>
-                    <li>
-                       <label for= "description">Описание:</label>
-                       <input className="input__text" type="text" id="description" name="description"/>
-                    </li>
-                    <li>
-                       <label for= "video">Видео:</label>
-                       <input type="file" id="video" name="video"/>
-                    </li>
-                    <li>
-                       <label for= "preview">Картинка на превю:</label>
-                       <input type="file" id="preview" name="preview"/>
-                    </li>
-                </ul>
-                <button className='create__botton' type='primery' htmlType="submit">
-                    <h1>Добавить </h1>
-                </button>
-            </form>
-            </div>
-        </div>
-    )
+  return (
+    <div className="content">
+      <div className="conteiner__category">
+        <form className="create__form" onSubmit={(event) => submit(event)}>
+          <ul>
+            <li>
+              <label for="name">Название:</label>
+              <input
+                className="input__text"
+                onChange={(e) => handle(e)}
+                value={date.name}
+                type="text"
+                id="name"
+                name="name"
+              />
+            </li>
+            <li>
+              <label for="description">Описание:</label>
+              <input
+                className="input__text"
+                onChange={(e) => handle(e)}
+                value={date.description}
+                type="text"
+                id="description"
+                name="description"
+              />
+            </li>
+            <li>
+              <label for="video">Видео:</label>
+              <input
+                onChange={(e) => handle(e)}
+                value={date.video}
+                type="file"
+                id="video"
+                name="video"
+              />
+            </li>
+            <li>
+              <label for="preview">Картинка на превю:</label>
+              <input
+                onChange={(e) => handle(e)}
+                value={date.preview}
+                type="file"
+                id="preview"
+                name="preview"
+              />
+            </li>
+          </ul>
+          <button className="create__botton">
+            <h1>Добавить </h1>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
--- video_social/frontend/src/components/video_details/video_details.js
+++ video_social/frontend/src/components/video_details/video_details.js
@@ -1,118 +1,135 @@
-import React,{useState,useEffect} from "react";
-import axios from "axios";
-import dateFormat from 'dateformat'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import dateFormat from 'dateformat';
 
-import './video_details.css'
+import './video_details.css';
 
+function VideoDetail({ match }) {
+  const url = 'http://127.0.0.1:8000/api/comment/';
+  const [video, setVideo] = useState([]);
+  const [comment, setComment] = useState([]);
+  
 
+  const id = match.params.id;
 
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `http://127.0.0.1:8000/api/videos/${id}/`,
+    }).then((response) => {
+      setVideo(response.data);
+      setComment(response.data.comments);
+    });
+  }, [id]);
+  const video_id = video.id;
+  
+  const [date, setDate] = useState({
+    name: '',
+    body: '',
+    video: '',
+  });
 
-function VideoDetail({match}){
+  console.log(date)
 
+  function submit(event) {
+    event.preventDefault();
+    axios
+      .post(url, {
+        name: date.name,
+        body: date.body,
+        video: date.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
 
-    const [video,setVideo]=useState([])
-    const [comment,setComment]=useState([])
-    
-    const id = match.params.id
+  function handle_commit(e) {
+    const newdate = { ...date };
+    newdate[e.target.id] = e.target.value;
+    setDate(newdate);
+    console.log(newdate);
+  }
 
-    useEffect(() => {
-        axios({
-            method:"GET",
-            url:`http://127.0.0.1:8000/api/videos/${id}/`
-        }).then(response=>{
-            setVideo(response.data)
-            setComment(response.data.comments)
-            
-            
-        })
-        
-    }, [id])
+  const caunter = comment.length;
+  
+  
+  return (
+    <div className="content">
+      <div className="conteiner__category">
+        <div className="video">
+          <video width="1200" height="800" controls="controls" poster={video.preview}>
+            <source src={video.video} type="video/mp4" />
+          </video>
+        </div>
+        <div className="video__detail-content">
+          <div className="video__detail-tittle">{video.name}</div>
+          <div className="video__detail-date">{dateFormat(video.published, 'mmmm dS,yyyy')}</div>
 
-    function handleFormSubmit (event){
-        event.preventDefault();
-        const body =event.target.elements.body.value;
-        const name =event.target.elements.name.value;
-        const video = video.id
-
-        console.log(body,name,video)
-    }
-    const caunter = comment.length
-    return(
-       <div className="content">
-           <div className="conteiner__category">
-            <div className="video">
-              <video width="1200" height="800" controls="controls" poster={video.preview}>
-                <source src={video.video} type="video/mp4"/>
-              </video>
-            </div>
-            <div className="video__detail-content">
-                <div className="video__detail-tittle">
-                    {video.name}
+          <div className="video__detail-user">
+            <img
+              className="user__avatar"
+              src="https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F_400x400.jpg"
+              alt=""
+            />
+            <div className="user__detail-name">{video.author}</div>
+          </div>
+          <div className="video__detail-description">{video.description}</div>
+          <div className="video__comments">
+            <h1 className="tittle">{caunter} коментариев : </h1>
+            {comment.map((com) => (
+              <div className="commets__details">
+                <div className="comments__header">
+                  <div className="comments__user">{com.name}</div>
+                  <div className="comments__date">{com.created}</div>
                 </div>
-                <div className="video__detail-date">
-                    {dateFormat(video.published,"mmmm dS,yyyy")}
-                </div>
-            
-
-                <div className="video__detail-user">
-                    <img className="user__avatar" src='https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F_400x400.jpg' alt=''/>
-                    <div className="user__detail-name">{video.author}</div>
-
-                </div>
-                <div className="video__detail-description">
-                    {video.description}
-                </div>
-                <div className="video__comments">
-                    <h1 className="tittle">{caunter} коментариев : </h1>
-                    {comment.map(com=>(
-                        <div className="commets__details">
-                            <div className="comments__header">
-                                <div className="comments__user">
-                                  {com.name}
-                                </div>
-                                <div className="comments__date">
-                                   {com.created}
-                                </div>
-                            </div>
-                            <div className="comments__body">
-                              {com.body}
-                            </div>
-                        
-                        
-                        
-
-                        </div>
-
-
-                    ))}
-                    <form className="comment__form" onSubmit={(event)=>handleFormSubmit(event)}>
-                        <ul>
-                            <li>
-                                <label for="name">Имя</label>
-                                <input className="input__text" type="text" id="name" name="name"/>
-
-                            </li>
-                            <li>
-                                <label for="body">Текст</label>
-                                <input className="input__body" type="text" id="body" name="body"/>
-
-
-                            </li>
-                        </ul>
-                        <button className='create__botton' type='primery' htmlType="submit">
-                            <h1>Добавить</h1>
-                            </button>
-
-                    </form>
-
-                </div>
-
-            </div>
-
-            
-           </div>
-       </div>
-    )
+                <div className="comments__body">{com.body}</div>
+              </div>
+            ))}
+            <form className="comment__form" onSubmit={(event) => submit(event)}>
+              <ul>
+                <li>
+                  <label for="name">Имя</label>
+                  <input
+                    className="input__text"
+                    onChange={(e) => handle_commit(e)}
+                    value={date.name}
+                    type="text"
+                    id="name"
+                    name="name"
+                  />
+                </li>
+                <li>
+                  <label for="body">Текст</label>
+                  <input
+                    className="input__body"
+                    onChange={(e) => handle_commit(e)}
+                    value={date.doby}
+                    type="text"
+                    id="body"
+                    name="body"
+                  />
+                </li>
+                <li>
+                  <input
+                    className="input__body"
+                    
+                    value={date.video}
+                    type="hidden"
+                    id="video"
+                    name="video"
+                  />
+                </li>
+              </ul>
+              <button className="create__botton" type="primery" htmlType="submit">
+                <h1>Добавить</h1>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoDetail;
